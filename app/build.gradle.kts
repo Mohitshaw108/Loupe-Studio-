44742e850c13e8a1b907e3bb58fcaee484ab7d6f
Mohitshaw108 <159328299+Mohitshaw108@users.noreply.github.com>
2026-08-08 17:33:29 +0530

    Update build.gradle.kts with resource options and dependencies
    Added noCompress option for tflite resources and updated dependencies for lifecycle-viewmodel-compose and tensorflow-lite.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+androidResources {
+        noCompress += "tflite"
+    }
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -41,6 +44,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("org.tensorflow:tensorflow-lite:2.14.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
